8bf597ab24aefccff9050e69517e045052509884
Nutty Nkosi <nkosin361@gmail.com>
2021-01-14 19:28:51 +0200

    Learning Angular Components 👨🏽‍💻

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,30 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'cards';
+  posts = [
+    {
+      imageUrl: 'assets/tree.jpeg',
+      title: 'Neat tree',
+      username: 'nature',
+      content: 'Saw this awesome tree during my hike today.'
+    },
+    {
+      imageUrl: 'assets/mountain.jpeg',
+      title: 'Snowy Mountains',
+      username: 'mountainlover',
+      content: 'Here is a picture of a snowy mountain.'
+    },
+    {
+      imageUrl: 'assets/biking.jpeg',
+      title: 'man biking',
+      username: 'bikingman',
+      content: 'Some dude biking.'
+    },
+    {
+      imageUrl: 'assets/biking.jpeg',
+      title: 'man biking',
+      username: 'bikingman',
+      content: 'Some dude biking.'
+    }
+  ];
 }
